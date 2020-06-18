dbb9a294e4c13eaeddc07d46faff91d902151cb3
Fujio Nakatsuka <com.nifty.cbm01799>
2020-06-18 19:16:14 +0900

    PHPhotoLibraryの使用、getImagesとurlリクエスト、APIキー、key wordの取得まで。

--- GglBksThumb01/SearchViewController.swift
+++ GglBksThumb01/SearchViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+import Photos
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var thumnailImageView: UIImageView!
+    
+    @IBOutlet weak var SearchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        PHPhotoLibrary.requestAuthorization{
+            (status) in
+            
+            switch(status){
+                case .authorized: break
+                case .denied: break
+                case .notDetermined: break
+                case .restricted: break
+                
+           }
+       }
     }
-
-
+//    検索ワードの値を元に画像を引っ張ってくる
+//(~はhttp)~s://www.googleapis.com/books/v1/volumes?~~~&q=Marc+Franz
+ // 配列は辞書型"items"で、その下にid,selfLink,title,authors,publisher,publishedDate,imageLinks{"thmbnail":"~://books.google.com/books/content?id=pfMRAQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"などがある。
+    func getImages(keyword:String){
+        
+        //API Key AIzaSyD7kgXX7s8FgC7RxNrRlvPSGqySdKIh8vQ
+    let url = "https://www.googleapis.com/books/v1/volumes?AIzaSyD7kgXX7s8FgC7RxNrRlvPSGqySdKIh8vQ&q=\(keyword)"
+        
+   //Alamofireでhttpリクエスト、値が返るのでJSON解析してImageView.imageに貼る
+        
+    }
+    
+    
+    
 }
 
